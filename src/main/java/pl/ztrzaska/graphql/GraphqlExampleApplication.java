c4ed5f779cc624115ec873a3add0a9484b9dbf42
Zdzisław Trzaska <zdzislaw.trzaska@asseco.pl>
2022-07-23 09:30:52 +0200

    Initial commit

--- src/main/java/pl/ztrzaska/graphql/GraphqlExampleApplication.java
+++ src/main/java/pl/ztrzaska/graphql/GraphqlExampleApplication.java
@@ -1,20 +1,12 @@
 package pl.ztrzaska.graphql;
 
-import graphql.kickstart.spring.web.boot.GraphQLWebsocketAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.web.context.ServletContextAware;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-import javax.servlet.ServletContext;
 import java.util.Locale;
 
-@SpringBootApplication(exclude = {
-        GraphQLWebsocketAutoConfiguration.class     //only for passing test Error creating bean serverEndpointExporter
-})
+@SpringBootApplication
 @ImportResource(value= "classpath:/package/hsql_cfg.xml")
 public class GraphqlExampleApplication {
 
