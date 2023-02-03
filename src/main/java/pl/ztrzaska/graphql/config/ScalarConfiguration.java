c2e9b7352db1371e870ee73712b8b97ccde457fc
Zdzisław Trzaska <zdzislaw.trzaska@asseco.pl>
2023-02-03 16:29:27 +0100

    scalar config

--- src/main/java/pl/ztrzaska/graphql/config/ScalarConfiguration.java
+++ src/main/java/pl/ztrzaska/graphql/config/ScalarConfiguration.java
@@ -1,7 +1,6 @@
 package pl.ztrzaska.graphql.config;
 
 import graphql.scalars.ExtendedScalars;
-import graphql.schema.GraphQLScalarType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
@@ -10,16 +9,6 @@ import java.util.List;
 
 @Configuration
 public class ScalarConfiguration {
-
-//    @Bean
-//    public GraphQLScalarType graphQLDate() {
-//        return ExtendedScalars.Date;
-//    }
-//
-//    @Bean
-//    public GraphQLScalarType graphQLDateTime() {
-//        return ExtendedScalars.DateTime;
-//    }
 
     @Bean
     public RuntimeWiringConfigurer runtimeWiringConfigurer() {
