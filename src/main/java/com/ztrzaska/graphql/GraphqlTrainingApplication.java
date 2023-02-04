2f78091ac72417420e08967f6909f15f48914e42
Zdzisław Trzaska <zdzislaw.trzaska@asseco.pl>
2023-02-04 17:44:53 +0100

    Mongo data initializator

--- src/main/java/com/ztrzaska/graphql/GraphqlTrainingApplication.java
+++ src/main/java/com/ztrzaska/graphql/GraphqlTrainingApplication.java
@@ -1,17 +1,37 @@
 package com.ztrzaska.graphql;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 import java.util.Locale;
 
 @SpringBootApplication
 public class GraphqlTrainingApplication {
 
+    @Value("classpath:data.json")
+    private Resource mongoData;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     public static void main(String[] args) {
         Locale.setDefault(Locale.ENGLISH); //hibernate validator messages language
         SpringApplication.run(GraphqlTrainingApplication.class, args);
+    }
+
+    @Bean
+    public Jackson2RepositoryPopulatorFactoryBean repositoryPopulator() {
+        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+        Resource[] resources = {mongoData};
+        factory.setMapper(objectMapper);
+        factory.setResources(resources);
+        return factory;
     }
 
 }
---
+++ src/main/java/com/ztrzaska/graphql/controller/ArticleRestController.java
@@ -0,0 +1,21 @@
+package com.ztrzaska.graphql.controller;
+
+import com.ztrzaska.graphql.model.ArticleDto;
+import com.ztrzaska.graphql.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ArticleRestController {
+
+    private final ArticleService articleService;
+
+    @GetMapping("/articles")
+    public List<ArticleDto> articles() {
+        return articleService.findAll();
+    }
+}
