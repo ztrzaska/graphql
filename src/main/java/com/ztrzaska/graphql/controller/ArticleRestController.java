7a63da0b9f8e7e9b2f1e3a376a172a5d544f8a7a
Zdzisław Trzaska <zdzislaw.trzaska@asseco.pl>
2023-02-04 18:56:06 +0100

    rest controller

--- src/main/java/com/ztrzaska/graphql/controller/ArticleRestController.java
+++ src/main/java/com/ztrzaska/graphql/controller/ArticleRestController.java
@@ -3,6 +3,7 @@ package com.ztrzaska.graphql.controller;
 import com.ztrzaska.graphql.model.ArticleDto;
 import com.ztrzaska.graphql.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ public class ArticleRestController {
 
     private final ArticleService articleService;
 
+    @CrossOrigin
     @GetMapping("/articles")
     public List<ArticleDto> articles() {
         return articleService.findAll();
