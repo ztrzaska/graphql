3c47fce1ee5e9374ae6080a4adcb3d4998d5f352
Zdzisław Trzaska <zdzislaw.trzaska@asseco.pl>
2023-02-04 18:56:52 +0100

    logger

--- src/main/java/com/ztrzaska/graphql/controller/ArticleRestController.java
+++ src/main/java/com/ztrzaska/graphql/controller/ArticleRestController.java
@@ -3,6 +3,7 @@ package com.ztrzaska.graphql.controller;
 import com.ztrzaska.graphql.model.ArticleDto;
 import com.ztrzaska.graphql.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 public class ArticleRestController {
 
@@ -18,6 +20,7 @@ public class ArticleRestController {
     @CrossOrigin
     @GetMapping("/articles")
     public List<ArticleDto> articles() {
+        log.info("Retrieving all articles");
         return articleService.findAll();
     }
 }
