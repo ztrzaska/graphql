06a8644fe6ec48d5be22edfa34f371856a8e68f1
Zdzisław Trzaska <zdzislaw.trzaska@asseco.pl>
2023-02-07 21:15:03 +0100

    valid

--- src/main/java/com/ztrzaska/graphql/resolver/mutation/BookMutation.java
+++ src/main/java/com/ztrzaska/graphql/resolver/mutation/BookMutation.java
@@ -1,19 +1,17 @@
 package com.ztrzaska.graphql.resolver.mutation;
 
+import com.ztrzaska.graphql.model.BookDto;
+import com.ztrzaska.graphql.model.input.CreateBookInputDto;
+import com.ztrzaska.graphql.service.BookService;
 import graphql.kickstart.tools.GraphQLMutationResolver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.annotation.Validated;
-import com.ztrzaska.graphql.model.BookDto;
-import com.ztrzaska.graphql.model.input.CreateBookInputDto;
-import com.ztrzaska.graphql.service.BookService;
 
 import javax.validation.Valid;
 
 @Slf4j
 @RequiredArgsConstructor
-@Validated
 @Component
 public class BookMutation implements GraphQLMutationResolver {
 
